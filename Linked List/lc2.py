224388684e3689b1ec749596cdd41d6e56585336
dodo <dodo_oschina@163.com>
2019-08-19 16:56:47 +0800

    add Linked List/lc2

---
+++ Linked List/lc2.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    '''
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    Example:
+    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+    Output: 7 -> 0 -> 8
+    Explanation: 342 + 465 = 807.
+
+    给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+    如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+    您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+    示例：
+    输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+    输出：7 -> 0 -> 8
+    原因：342 + 465 = 807
+
+    '''
+
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummyHead = ListNode(0)
+        lastNode = dummyHead
+        carry = 0
+        while carry or l1 or l2:
+            val, carry = divmod(carry + (l1.val if l1 else 0) + (l2.val if l2 else 0), 10)
+            lastNode.next = ListNode(val)
+            lastNode = lastNode.next
+            l1 = l1.next
+            l2 = l2.next
+        return dummyHead.next
