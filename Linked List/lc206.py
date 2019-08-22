1d448f04618585a6c2d3bc6df64e4af4204ed8c1
dodo <dodo_oschina@163.com>
2019-08-22 11:13:23 +0800

    add Linked List/lc206,lc876

---
+++ Linked List/lc206.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    '''
+    Reverse a singly linked list.
+    Example:
+    Input: 1->2->3->4->5->NULL
+    Output: 5->4->3->2->1->NULL
+    Follow up:
+    A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+    反转一个单链表。
+    示例:
+    输入: 1->2->3->4->5->NULL
+    输出: 5->4->3->2->1->NULL
+    进阶:
+    你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+    '''
+
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        while head:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+        return prev
---
+++ Linked List/lc876.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    '''
+    Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+    If there are two middle nodes, return the second middle node.
+
+    给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+    如果有两个中间结点，则返回第二个中间结点
+    '''
+
+    def middleNode(self, head: ListNode) -> ListNode:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
